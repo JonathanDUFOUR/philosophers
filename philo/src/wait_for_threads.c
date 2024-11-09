0dc4487387c2c2b327e5ff1213b870dc25747a11
jodufour <jodufour@student.42.fr>
2024-11-09 02:48:05 +0100

    docs: updated the doxygen comment of the `wait_for_threads` function

--- philo/src/wait_for_threads.c
+++ philo/src/wait_for_threads.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 02:36:53 by jodufour          #+#    #+#             */
-/*   Updated: 2024/11/09 00:59:13 by jodufour         ###   ########.fr       */
+/*   Updated: 2024/11/09 02:29:21 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 /**
  * @brief Waits for given threads to finish.
  * 
- * @param threads An array of threads to wait for. It is assumed
+ * @param thread_ids The ids of the threads to wait for. It is assumed
  *        that the threads are all joinable.
  * 
- * @param number_of_threads How many elements does `threads` contain.
+ * @param number_of_threads How many elements does `thread_ids` contain.
  */
 void
 	wait_for_threads(
